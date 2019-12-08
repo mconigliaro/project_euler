6e062aedda74916cfab6bdae343d6afe03b29138
Mike Conigliaro <mike@conigliaro.org>
2019-12-08 13:31:09 -0700

    Add problem 35

---
+++ problems/035.py
@@ -0,0 +1,23 @@
+from collections import deque
+from project_euler.prime import primes, is_prime
+
+
+def rotations(n):
+    if n < 10:
+        return [n]
+    else:
+        r = []
+        digits = deque(i for i in list(str(n)))
+        for i in range(len(digits) - 1):
+            digits.append(digits.popleft())
+            r.append(int(''.join(digits)))
+        return r
+
+
+def solve():
+    return sum(1 for i in primes(up_to=1000000)
+               if all(is_prime(r) for r in rotations(i)))
+
+
+def test():
+    assert solve() == 55
