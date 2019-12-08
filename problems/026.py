0023b72ff7c70017cfca24868ba94d2068330fe0
Mike Conigliaro <mike@conigliaro.org>
2019-12-07 17:53:43 -0700

    Add problem 26

---
+++ problems/026.py
@@ -0,0 +1,19 @@
+from project_euler.prime import primes
+from decimal import getcontext, Decimal
+import re
+
+
+def solve():
+    longest = (0, 0)
+    getcontext().prec = 2048
+    for i in primes(up_to=1000):
+        match = re.search(r'(\d{2,}?)(?=\1)+', str(Decimal(1) / i))
+        if match:
+            size = len(match.group())
+            if size > longest[1]:
+                longest = (i, size)
+    return longest[0]
+
+
+def test():
+    assert solve() == 983
