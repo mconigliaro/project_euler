dd946bf5628d373097abacb85652819605fa6d1c
Mike Conigliaro <mike@conigliaro.org>
2019-12-09 16:15:31 -0700

    Add pentagonal number methods

--- project_euler/figurate.py
+++ project_euler/figurate.py
@@ -1,6 +1,25 @@
+from math import sqrt
+
+
 def is_triangular(n):
-    return (8 * n + 1) ** 0.5 % 1 == 0
+    if n == 0:
+        return True
+    else:
+        x = (sqrt(8 * n + 1) - 1) / 2
+        return x == int(x)
 
 
 def triangular(n):
     return int(n * (n + 1) / 2)
+
+
+def is_pentagonal(n):
+    if n == 0:
+        return True
+    else:
+        x = (sqrt(24 * n + 1) + 1) / 6
+        return x == int(x)
+
+
+def pentagonal(n):
+    return int(n * (3 * n - 1) / 2)
--- tests/test_figurate.py
+++ tests/test_figurate.py
@@ -1,5 +1,6 @@
 import pytest
-from project_euler.figurate import is_triangular, triangular
+from project_euler.figurate import is_triangular, triangular, \
+  is_pentagonal, pentagonal
 
 
 @pytest.fixture
@@ -8,7 +9,33 @@ def known_triangular():
             153, 171, 190, 210, 231, 253, 276, 300, 325]
 
 
-def test_triangular(known_triangular):
+@pytest.fixture
+def known_not_triangular(known_triangular):
+    return list(set(range(known_triangular[-1])) - set(known_triangular))
+
+
+@pytest.fixture
+def known_pentagonal():
+    return [0, 1, 5, 12, 22, 35, 51, 70, 92, 117, 145, 176, 210, 247, 287, 330,
+            376, 425, 477, 532, 590, 651, 715, 782, 852, 925, 1001]
+
+
+@pytest.fixture
+def known_not_pentagonal(known_pentagonal):
+    return list(set(range(known_pentagonal[-1])) - set(known_pentagonal))
+
+
+def test_triangular(known_triangular, known_not_triangular):
     for i, x in enumerate(known_triangular):
         assert triangular(i) == x
         assert is_triangular(x)
+
+    assert not any(is_triangular(x) for x in known_not_triangular)
+
+
+def test_pentagonal(known_pentagonal, known_not_pentagonal):
+    for i, x in enumerate(known_pentagonal):
+        assert pentagonal(i) == x
+        assert is_pentagonal(x)
+
+    assert not any(is_pentagonal(x) for x in known_not_pentagonal)
