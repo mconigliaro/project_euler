3c910efddb4d49766dd01585201678a6363c9bac
Mike Conigliaro <mike@conigliaro.org>
2019-12-10 16:28:12 -0700

    Refactor

--- problems/049.py
+++ problems/049.py
@@ -4,18 +4,13 @@ from project_euler.prime import primes
 def solve():
     p = [p for p in primes(end=9999)
          if p >= 1000 and p not in [1487, 4817, 8147]]
-
     d = {}
     for n in p:
-        key = ''.join(sorted(str(n)))
-        d.setdefault(key, []).append(n)
-
-    chunk_by = 3
-    for s in [v for v in d.values() if len(v) >= chunk_by]:
-        chunks = len(s) - chunk_by + 1
-        for i in range(chunks):
-            if s[i + 2] - s[i + 1] == s[i + 1] - s[i]:
-                return int(''.join(str(x) for x in s[i:i + chunk_by]))
+        d.setdefault(''.join(sorted(str(n))), []).append(n)
+    for s in [v for v in d.values() if len(v) >= 3]:
+        for offset in range(len(s) - 3 + 1):
+            if s[offset + 2] - s[offset + 1] == s[offset + 1] - s[offset]:
+                return int(''.join(str(x) for x in s[offset:offset + 3]))
 
 
 def test():
