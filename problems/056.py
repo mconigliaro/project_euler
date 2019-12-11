d7e9f40f447ff1932577ff2963b5bdc312993238
Mike Conigliaro <mike@conigliaro.org>
2019-12-11 14:35:58 -0700

    Add problem 56

---
+++ problems/056.py
@@ -0,0 +1,12 @@
+def solve():
+    m = 0
+    for i in range(100):
+        for j in range(100):
+            s = sum(int(x) for x in list(str(i ** j)))
+            if s > m:
+                m = s
+    return m
+
+
+def test():
+    assert solve() == 972
