b30c91b4ae3783d95542e8619c3bdb2231ee5926
Mike Conigliaro <mike@conigliaro.org>
2019-12-15 22:49:01 -0700

    Trivial

--- project_euler/pythagoras.py
+++ project_euler/pythagoras.py
@@ -8,10 +8,9 @@ def pythagorean_triplets(end, primitive=False):
         b_start = a + 1
         b_end = int(sqrt((end ** 2) - (a ** 2))) + 1
         for b in range(b_start, b_end):
-            if not primitive or \
-                  (primitive and gcd(a, b) == 1):
+            if not primitive or (primitive and gcd(a, b) == 1):
                 c_start = int(sqrt(a ** 2 + b ** 2))
-                c_end = int(sqrt(a ** 2 + b ** 2)) + 2
+                c_end = c_start + 2
                 for c in range(c_start, c_end):
                     if c ** 2 == a ** 2 + b ** 2:
                         triplet = tuple(sorted([a, b, c]))
