4ae1b5f93d3d8e147694efbff5646716134abb37
Mike Conigliaro <mike@conigliaro.org>
2020-04-04 16:41:08 -0600

    Use itertools

--- problems/004.py
+++ problems/004.py
@@ -1,12 +1,13 @@
+import itertools as it
+
+
 def solve():
-    r = range(100, 999)
     largest = 0
-    for x in r:
-        for y in r:
-            product = x * y
-            r_product = int(str(product)[::-1])
-            if product == r_product and product > largest:
-                largest = product
+    for x, y in it.product(range(100, 999), repeat=2):
+        product = x * y
+        r_product = int(str(product)[::-1])
+        if product == r_product and product > largest:
+            largest = product
     return largest
 
 
