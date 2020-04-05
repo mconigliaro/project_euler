4d4d301fb5f310c7c5cd5fb15335938b1b708a68
Mike Conigliaro <mike@conigliaro.org>
2020-04-04 21:41:03 -0600

    Use "with" statement

--- project_euler/data.py
+++ project_euler/data.py
@@ -1,5 +1,3 @@
 def data(file):
-    f = open(file, 'r')
-    lines = [l.strip() for l in f.readlines()]
-    f.close()
-    return lines
+    with open(file, 'r') as f:
+        return [l.strip() for l in f.readlines()]
